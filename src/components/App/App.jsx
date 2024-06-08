502e719a4cacdf68461cdcebfb230c3cbecd82da
Serhii Vasiliev <sepovi4@gmail.com>
2024-06-08 16:36:01 +0200

    update3.2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,25 +1,14 @@
-import { useSelector } from "react-redux";
-
 import ContactForm from "../ContactForm/ContactForm";
 import SearchBox from "../SearchBox/SearchBox";
 
 import ContactList from "../ContactList/ContactList";
-import { selectContacts } from "../../redux/contactsSlice";
-import { selectNameFilter } from "../../redux/filtersSlice";
 
 export default function App() {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectNameFilter);
-
-  const visibleContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
   return (
     <div>
       <ContactForm />
       <SearchBox />
-      <ContactList contacts={visibleContacts} />
+      <ContactList />
     </div>
   );
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,20 @@
 import css from "../ContactList/ContactList.module.css";
 import Contact from "../Contact/Contact";
 
-export default function ContactList({ contacts }) {
+import { useSelector } from "react-redux";
+import { selectContacts } from "../../redux/contactsSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
+
+export default function ContactList() {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+
+  const visibleContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
   return (
     <ul className={css.uList}>
-      {contacts.map((contact) => (
+      {visibleContacts.map((contact) => (
         <li className={css.list} key={contact.id}>
           <Contact data={contact} />
         </li>
