a0f47f818bfb8f43d50e2f5d938218f46efab50c
Serhii Vasiliev <sepovi4@gmail.com>
2024-06-21 11:34:27 +0200

    update3.5

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,10 +3,10 @@ import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import { refreshUser } from "../../redux/auth/operations";
 import Layout from "../Layout/Layout";
-import HomePage from "../../Pages/HomePage/HomePage";
-import RegisterPage from "../../Pages/RegisterPage/RegisterPage";
-import LoginPage from "../../Pages/LoginPage/LoginPage";
-import ContactsPage from "../../Pages/ContactsPage/ContactsPage";
+import HomePage from "../../pages/HomePage/HomePage";
+import RegisterPage from "../../pages/RegisterPage/RegisterPage";
+import LoginPage from "../../pages/LoginPage/LoginPage";
+import ContactsPage from "../../pages/ContactsPage/ContactsPage";
 import PrivateRoute from "../PrivateRoute";
 import RestrictedRoute from "../RestrictedRuote";
 
